56598ccdefdf4e856c12620f88bc64aa53234f52
TomasNagy <tomi.nagy@outlook.com>
2023-05-29 12:01:23 +0200

    imp!: insertion sort algorithm

--- insertion_sort.c
+++ insertion_sort.c
@@ -5,4 +5,18 @@
 int main(void)
 {
     int unsorted_array[ARRAY_LENGTH] = {1, 0, 4, 8, 10, 3, 9, 7, 2, 6, 5};
+
+    for (int i = 0; i < ARRAY_LENGTH-1; i++)
+    {
+        int j = i;
+            
+        while (j >= 0 && unsorted_array[j] > unsorted_array[j + 1])    
+        {
+            int tmp = unsorted_array[j + 1];
+            unsorted_array[j + 1] = unsorted_array[j];
+            unsorted_array[j] = tmp;
+
+            j--;
+        }
+    }
 }
