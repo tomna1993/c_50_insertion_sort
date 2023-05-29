32f455e993a1588eaca8ef87e376de6e257031c8
TomasNagy <tomi.nagy@outlook.com>
2023-05-29 12:16:30 +0200

    refactor: move insertion sort alg. into function
    Move insertion sort algorithm from main to its separate
    function.

--- insertion_sort.c
+++ insertion_sort.c
@@ -2,26 +2,33 @@
 
 #define ARRAY_LENGTH 11
 
+void insertion_sort(int unsorted_list_array[], int array_length);
+
 int main(void)
 {
     int unsorted_array[ARRAY_LENGTH] = {1, 0, 4, 8, 10, 3, 9, 7, 2, 6, 5};
 
-    for (int i = 0; i < ARRAY_LENGTH-1; i++)
-    {
-        int j = i;
-            
-        while (j >= 0 && unsorted_array[j] > unsorted_array[j + 1])    
-        {
-            int tmp = unsorted_array[j + 1];
-            unsorted_array[j + 1] = unsorted_array[j];
-            unsorted_array[j] = tmp;
-
-            j--;
-        }
-    }
+    insertion_sort(unsorted_array, ARRAY_LENGTH);
 
     for (int i = 0; i < ARRAY_LENGTH; i++)
     {
         printf ("%i ", unsorted_array[i]);
+    }
+}
+
+void insertion_sort(int unsorted_list_array[], int array_length)
+{
+    for (int i = 0; i < array_length-1; i++)
+    {
+        int j = i;
+            
+        while (j >= 0 && unsorted_list_array[j] > unsorted_list_array[j + 1])    
+        {
+            int tmp = unsorted_list_array[j + 1];
+            unsorted_list_array[j + 1] = unsorted_list_array[j];
+            unsorted_list_array[j] = tmp;
+
+            j--;
+        }
     }
 }
